cce33e70ae180e09df447da22d7483dfd363ef5e
syedhaseeb379 <syedhaseeb379@gmail.com>
2018-04-14 07:22:36 +0000

    Added FeedService

--- app/data/FeedResponse.java
+++ app/data/FeedResponse.java
@@ -1,5 +1,10 @@
 package data;
 
 public class FeedResponse {
+    public String title;
+    public String description;
+    public String pubDate;
+
 
 }
+
--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -3,6 +3,7 @@ package services;
 import data.FeedResponse;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import play.libs.XML;
 import play.libs.ws.WS;
 import play.libs.ws.WSRequest;
 import play.libs.ws.WSResponse;
@@ -11,5 +12,24 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class FeedService {
+    public FeedResponse getFeedByQuery(String query){
+        FeedResponse feedResponseObject=new FeedResponse();
+        try {
+            WSRequest feedRequest=WS.url("https://news.google.com/news");
+            CompletionStage<WSResponse> responsePromise=feedRequest
+                    .setQueryParameter("q",query)
+                    .setQueryParameter("output","rss")
+                    .get();
+            Document feedresponse=responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+            Node item=feedresponse.getFirstChild().getChildNodes().item(10);
+            feedResponseObject.title=item.getChildNodes().item(0).getFirstChild().getNodeValue();
+            feedResponseObject.pubDate=item.getChildNodes().item(3).getFirstChild().getNodeValue();
+            feedResponseObject.description=item.getChildNodes().item(4).getFirstChild().getNodeValue();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
 }
